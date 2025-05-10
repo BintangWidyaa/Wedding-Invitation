bb0cb26340fa6bd4245312b7b4d5aa46f8e2c52a
unknown <youkata666@gmail.com>
2025-05-11 03:23:55 +0700

    X V MMXXV

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  publicDir: "public",
+  root: "./",
+  base:"/Wedding-Invitation/",
+  build: {
+    outDir: "dist",
+  },
+});
