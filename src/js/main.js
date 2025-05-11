3b9422509d56a581c0e2d82363b53c9aa07213bf
unknown <youkata666@gmail.com>
2025-05-11 13:36:20 +0700

    [-] apakah done?

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,5 @@
 import "../css/fonts.css";
 import "../css/main.css";
 
-// pakai alias custom @fortawesome/css
-// import "@fortawesome/fontawesome-free/css/all.min.css";
-
-// swiper
-import "swiper/swiper.min.css";
-
 // script.js
 import "./script.js";
